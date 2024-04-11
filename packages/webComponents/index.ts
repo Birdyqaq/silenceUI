f32229795fbf51c3776821416c409bc2e9948c28
wuzheng001 <1962443189@qq.com>
2024-04-12 02:33:13 +0800

    待梳理完善button与unocss结合

--- packages/webComponents/index.ts
+++ packages/webComponents/index.ts
@@ -1,2 +1,3 @@
 console.log(11)
 import 'uno.css'
+import "./components/button/index.ts"
--- packages/webComponents/uno.config.ts
+++ packages/webComponents/uno.config.ts
@@ -1,10 +1,18 @@
 //uno.config.ts
 
-import { defineConfig } from "unocss";
-
+import { defineConfig, presetUno } from "unocss";
 export default defineConfig({
+    presets: [
+        presetUno(),
+    ],
     //...Unocss的配置项 看下面配置介绍
     rules: [
         [/^m-([\.\d]+)$/, ([_, num]) => ({ margin: `${num}px` })],
+        ['primary', { "background-color": "blue" }],
+        ['secondary', { "background-color": "green" }],
+        // 自定义 UnoCSS 规则
+        // [/^bg-blue-500$/, ("background-color: blue !important;")],
+        // 添加更多自定义规则...
+
     ],
 });
